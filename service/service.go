7e1a22f692f664bae51c320f1afdccfc7812bb8c
Linke, Mario <Mario.Linke@ing-diba.de>
2018-06-18 15:54:33 +0200

    add doc (links) and appengine.log

--- service/service.go
+++ service/service.go
@@ -1,3 +1,22 @@
+/*
+ Package service is the interface to interact with Heroes
+
+ Golang Doc for App Engine:
+	https://cloud.google.com/appengine/docs/standard/go/reference
+	https://github.com/GoogleCloudPlatform/go-endpoints
+
+ Cron:
+	https://cloud.google.com/appengine/docs/standard/go/config/cron
+ Taskqueue:
+	https://cloud.google.com/appengine/docs/standard/go/taskqueue/
+ PubSub:
+ 	https://cloud.google.com/pubsub/docs/overview
+	https://godoc.org/cloud.google.com/go/pubsub
+
+
+ Samples:
+	https://github.com/GoogleCloudPlatform/golang-samples/
+*/
 package service
 
 import (
@@ -6,6 +25,7 @@ import (
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
 const (
@@ -15,13 +35,16 @@ const (
 	KIND = "Hero"
 )
 
+// Hero the struct
 type Hero struct {
 	ID   int64  `json:"id" datastore:"-"`
 	Name string `json:"name"`
 }
 
+// HeroService access to Heroes methods
 type HeroService struct{}
 
+// List get all Heros
 func (HeroService) List(c context.Context) ([]Hero, error) {
 	c, err := appengine.Namespace(c, NAMESPACE)
 	if err != nil {
@@ -29,9 +52,15 @@ func (HeroService) List(c context.Context) ([]Hero, error) {
 	}
 
 	heroes := []Hero{}
-	_, err = datastore.NewQuery(KIND).Order("ID").GetAll(c, &heroes)
+	keys, err := datastore.NewQuery(KIND).Order("ID").GetAll(c, &heroes)
 	if err != nil {
+		log.Errorf(c, "----- Err by datastore.GetAll: %v", err)
 		return heroes, fmt.Errorf("Err by datastore.GetAll: %v", err)
+	}
+
+	log.Infof(c, "----- Find %v Keys for Heroes", len(keys))
+	for i, k := range keys {
+		heroes[i].ID = k.IntID()
 	}
 
 	return heroes, nil
